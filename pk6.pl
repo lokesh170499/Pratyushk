19ef5a142d1bf8d6ad78e65b2b720641ebbd3023
170970 <57993317+170970@users.noreply.github.com>
2020-01-21 15:54:33 +0530

    Add files via upload

---
+++ pk6.pl
@@ -0,0 +1,18 @@
+#To use the conditional statement in Pearl
+#!usr/bin/perl-w
+#if-elsif-else
+$word='INSTRUMENTATION';
+if($word eq 'PRATYUSH')
+{
+print"PRATYUSH\n";
+}
+elsif($word eq 'INSTRUMENTATION')
+{print"INSTRUMENTATION\n";
+}
+else
+{print"I am undergraduate\n";
+}
+print"..............................................";
+print"\n\n";
+#exit
+
