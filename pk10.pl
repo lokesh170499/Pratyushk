5fda1c23af6293815c2f9940aa4047bc90691483
170970 <57993317+170970@users.noreply.github.com>
2020-01-21 16:09:43 +0530

    Add files via upload

---
+++ pk10.pl
@@ -0,0 +1,7 @@
+# TO PERFORM VARIOUS ARRAY OPERATIONS USING PERL
+#!usr/bin/perl-w
+@bases=('A','G','C','T');
+@reverse=reverse@bases;
+print"here's is the bases in reverse:";
+print"@reverse\n\n";
+
