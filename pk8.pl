c30597089b8df7a8fb063206a3741a8986fa0413
170970 <57993317+170970@users.noreply.github.com>
2020-01-21 15:57:22 +0530

    Add files via upload

---
+++ pk8.pl
@@ -0,0 +1,9 @@
+# TO PERFORM VARIOUS ARRAY OPERATIONS USING PERL
+#!usr/bin/perl-w
+@bases=('A','G','C','T');
+$base1=shift @bases;
+print"Here's the element remove from the end"; 
+print $base1,"\n\n";
+print"Here is the remaining array of the bases";
+print"@bases\n";
+
