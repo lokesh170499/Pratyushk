cc7579c5063ad4870a7e6a4fa16e877b73f30af2
170970 <57993317+170970@users.noreply.github.com>
2020-01-28 17:19:17 +0530

    Add files via upload

---
+++ pk2.pl
@@ -0,0 +1,10 @@
+$DNA1='AGCTAG';
+print"1st dna sequence : $DNA1\n";
+$DNA2='ACTAGC';
+print" 2nd dna sequence : $DNA2\n";
+$DNACON1="$DNA1$DNA2";
+print"DNACON1:$DNACON1\n";
+$DNACON2="$DNA1.$DNA2";
+print"DNACON2:$DNACON2\n";
+exit;
+
